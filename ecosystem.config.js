4194b36a784322979057d0053c931ad114999d97
Raghuram Iyer <riyer@fleetstudio.com>
2018-09-07 17:01:38 +0530

    ported to github

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     production: {
       user: 'fitkits-rpt',
       host: '142.93.208.162',
-      ref: 'origin/master',
+      ref: 'github_origin/master',
       repo: 'https://gitlab.com/ragzzyr/fitkits-admin.git',
       path: '/home/fitkits-rpt/admin'
     }
